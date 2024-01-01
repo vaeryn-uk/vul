6f94320004cf3cbe3e863037bb0dd3cdac5a62e1
Andy Cook <andrewcook1990@gmail.com>
2024-01-01 16:43:14 +0100

    Some world-based accessors with checks

---
+++ VulRuntime/Private/World/VulWorldGlobals.cpp
@@ -0,0 +1,8 @@
+﻿#include "World/VulWorldGlobals.h"
+
+UWorld* VulRuntime::WorldGlobals::GetWorldChecked(UObject* Object)
+{
+	auto World = Object->GetWorld();
+	checkf(IsValid(World), TEXT("Object does not have a valid world"))
+	return World;
+}
---
+++ VulRuntime/Public/World/VulWorldGlobals.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+
+/**
+ * Provides utility accessors to classes & functionality via a UWorld.
+ */
+namespace VulRuntime::WorldGlobals
+{
+	/**
+	 * Gets a UWorld from the given UObject, checking that it exists/is valid.
+	 */
+	VULRUNTIME_API UWorld* GetWorldChecked(UObject* Object);
+
+	/**
+	 * Gets a GameInstance subsystem of the requested type from the given UObject's world, checking that it exists/is valid.
+	 */
+	template <typename Subsystem>
+	Subsystem* GetGameInstanceSubsystemChecked(UObject* Ctx)
+	{
+		auto SS = GetWorldChecked(Ctx)->GetGameInstance()->GetSubsystem<Subsystem>();
+		checkf(IsValid(SS), TEXT("Cannot find GameInstance subsystem"))
+		return SS;
+	}
+
+	/**
+	 * Gets a GameState of the requested type from the given UObject's world, checking that it exists/is valid.
+	 */
+	template <typename GameState>
+	GameState* GetGameStateChecked(UObject* Ctx)
+	{
+		auto GS = GetWorldChecked(Ctx)->GetGameState<GameState>();
+		checkf(IsValid(GS), TEXT("Cannot find GameState"))
+		return GS;
+	}
+
+	/**
+	 * Gets the first player controller of the requested type from the given UObject's world, checking that it exists/is valid.
+	 */
+	template <typename PlayerController>
+	PlayerController* GetFirstPlayerController(UObject* Ctx)
+	{
+		auto PC = Cast<PlayerController>(GetWorldChecked(Ctx)->GetFirstPlayerController());
+		checkf(IsValid(PC), TEXT("Cannot find player controller"))
+		return PC;
+	}
+}
