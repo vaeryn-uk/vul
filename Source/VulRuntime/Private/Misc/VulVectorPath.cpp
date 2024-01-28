6b1f09f7ca32f7fec975925f93c3778387dc5494
Andy Cook <andrewcook1990@gmail.com>
2024-01-28 17:17:14 +0100

    Path-travelling algorithm

---
+++ Source/VulRuntime/Private/Misc/VulVectorPath.cpp
@@ -0,0 +1,55 @@
+﻿#include "Misc/VulVectorPath.h"
+
+FVector FVulVectorPath::Interpolate(const float Alpha) const
+{
+    if (!IsValid())
+	{
+		return FVector::Zero();
+	}
+
+	auto Remaining = Distance * FMath::Clamp(Alpha, 0, 1);
+
+	for (auto I = 1; I < Points.Num(); ++I)
+	{
+		const auto Segment = (Points[I] - Points[I - 1]);
+
+		if (Segment.Length() <= Remaining)
+		{
+			Remaining -= Segment.Length();
+			continue;
+		}
+
+		return Points[I - 1] + Remaining / Segment.Length() * Segment;
+	}
+
+	return Points.Last();
+}
+
+float FVulVectorPath::GetDistance() const
+{
+	return Distance;
+}
+
+bool FVulVectorPath::IsValid() const
+{
+	// Need at least two points for this to be valid.
+	return Points.Num() > 1;
+}
+
+void FVulVectorPath::CalculateDistance()
+{
+	Distance = 0;
+
+	if (!IsValid())
+	{
+		return;
+	}
+
+	for (auto I = 1; I < Points.Num(); ++I)
+	{
+		const auto Prev = Points[I - 1];
+		const auto Curr = Points[I];
+
+		Distance += (Curr - Prev).Length();
+	}
+}
---
+++ Source/VulRuntime/Public/Misc/VulVectorPath.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "VulVectorPath.generated.h"
+
+/**
+ * Models a path in the world that represents an actor's path of travel.
+ */
+USTRUCT()
+struct VULRUNTIME_API FVulVectorPath
+{
+	GENERATED_BODY()
+
+	FVulVectorPath() = default;
+
+	/**
+	 * InPoints must be at least two points for this structure to be valid.
+	 */
+	FVulVectorPath(const TArray<FVector>& InPoints) : Points(InPoints)
+	{
+		CalculateDistance();
+	}
+
+	/**
+	 * Returns the position along this path, where Alpha is a value between 0 (the start) and 1 (the end).
+	 *
+	 * Alpha will be clamped.
+	 */
+	FVector Interpolate(const float Alpha) const;
+
+	/**
+	 * The total distance covered when traversing the full path.
+	 */
+	float GetDistance() const;
+
+	/**
+	 * True if this path has at least 2 points.
+	 */
+	bool IsValid() const;
+
+private:
+	TArray<FVector> Points;
+
+	/**
+	 * Cached total distance.
+	 */
+	float Distance;
+
+	void CalculateDistance();
+};
