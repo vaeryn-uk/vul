2ce193a132ab7b0d5f96c498bbcdd65cff59c16b
Andy Cook <andrewcook1990@gmail.com>
2024-05-11 03:56:21 +0200

    Invisible button wrapper widget
    To make things clickable whilst still leveraging Unreal's button logic &
    functionality.

---
+++ Source/VulRuntime/Private/UserInterface/VulWrapButton.cpp
@@ -0,0 +1,22 @@
+﻿#include "UserInterface/VulWrapButton.h"
+#include "Blueprint/WidgetTree.h"
+
+UVulWrapButton* UVulWrapButton::WrapWidget(UUserWidget* Owner, UWidget* ToWrap)
+{
+	const auto Wrapper = Owner->WidgetTree->ConstructWidget<UVulWrapButton>();
+	Wrapper->SetContent(ToWrap);
+	return Wrapper;
+}
+
+TSharedRef<SWidget> UVulWrapButton::RebuildWidget()
+{
+	SetBackgroundColor(FLinearColor::Transparent);
+	OnClicked.AddUniqueDynamic(this, &UVulWrapButton::UVulWrapButton::TriggerWrapButtonClicked);
+
+	return Super::RebuildWidget();
+}
+
+void UVulWrapButton::TriggerWrapButtonClicked()
+{
+	WrapButtonClicked.Broadcast(GetContent());
+}
---
+++ Source/VulRuntime/Public/UserInterface/VulWrapButton.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Button.h"
+#include "UObject/Object.h"
+#include "VulWrapButton.generated.h"
+
+/**
+ * A button that wraps a single child purely to provide click behaviour.
+ *
+ * This is an invisible widget and makes non-button elements click-able with
+ * a button widget which has proper click-detection logic. This is better
+ * solution that implementing OnNativeMouseDown or similar in your widget
+ * directly.
+ */
+UCLASS()
+class VULRUNTIME_API UVulWrapButton : public UButton
+{
+	GENERATED_BODY()
+
+public:
+	static UVulWrapButton* WrapWidget(UUserWidget* Owner, UWidget* ToWrap);
+
+	DECLARE_MULTICAST_DELEGATE_OneParam(FVulWrapButtonClicked, UWidget*)
+
+	/**
+	 * Attach listeners here. This will be invoked with the widget this button
+	 * wraps.
+	 */
+	FVulWrapButtonClicked WrapButtonClicked;
+
+protected:
+	virtual TSharedRef<SWidget> RebuildWidget() override;
+
+private:
+	UFUNCTION()
+	void TriggerWrapButtonClicked();
+};
