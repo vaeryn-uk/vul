792ec7855fcd9057aaaff135a0554f5350ce90ef
Andy Cook <andrewcook1990@gmail.com>
2025-05-20 01:24:55 +0200

    Reinstate tests

--- Source/VulRuntime/Private/Field/Tests/TestVulFieldMeta.cpp
+++ Source/VulRuntime/Private/Field/Tests/TestVulFieldMeta.cpp
@@ -37,160 +37,160 @@ bool TestDescribe(VulTest::TC TC, const FVulFieldSet& Set, FVulFieldSerializatio
 
 bool TestVulFieldMeta::RunTest(const FString& Parameters)
 {
-// 	VulTest::Case(this, "Schema generation - basic object", [](VulTest::TC TC)
-// 	{
-// 		FVulFieldSerializationContext Ctx;
-// 		TSharedPtr<FVulFieldDescription> Desc = MakeShared<FVulFieldDescription>();
-// 		VTC_MUST_EQUAL(true, TestDescribe<FVulTestFieldType>(TC, Ctx, Desc), "");
-//
-// 		const auto JsonSchema = VulRuntime::Field::JsonToString(Desc->JsonSchema());
-//
-// 		FString Expected = R"(
-// {
-// 	"type": "object",
-// 	"properties": {
-// 		"bool": {"type": "boolean"},
-// 		"int": {"type": "number"},
-// 		"string": {"type": "string"},
-// 		"map": {
-// 			"type": "object",
-// 			"additionalProperties": {"type": "number"}
-// 		},
-// 		"array": {
-// 			"type": "array",
-// 			"items": {"type": "boolean"}
-// 		}
-// 	}
-// })";
-// 		
-// 		(void)TC.JsonObjectsEqual(JsonSchema, Expected);
-// 	});
-// 	
-// 	VulTest::Case(this, "Schema generation - nested objects", [](VulTest::TC TC)
-// 	{
-// 		FVulFieldSerializationContext Ctx;
-// 		TSharedPtr<FVulFieldDescription> Desc = MakeShared<FVulFieldDescription>();
-// 		VTC_MUST_EQUAL(true, TestDescribe<FVulTestFieldParent>(TC, Ctx, Desc), "");
-//
-// 		const auto JsonSchema = VulRuntime::Field::JsonToString(Desc->JsonSchema());
-//
-// 		FString Expected = R"(
-// {
-// 	"type": "object",
-// 	"properties": {
-// 		"inner": {
-// 			"type": "object",
-// 			"properties": {
-// 				"bool": {"type": "boolean"},
-// 				"int": {"type": "number"},
-// 				"string": {"type": "string"},
-// 				"map": {
-// 					"type": "object",
-// 					"additionalProperties": {"type": "number"}
-// 				},
-// 				"array": {
-// 					"type": "array",
-// 					"items": {"type": "boolean"}
-// 				}
-// 			}
-// 		}
-// 	}
-// })";
-// 		
-// 		(void)TC.JsonObjectsEqual(JsonSchema, Expected);
-// 	});
-// 	
-// 	VulTest::Case(this, "Schema generation - type cannot be described", [](VulTest::TC TC)
-// 	{
-// 		class FMyCustomType : FString { };
-// 		
-// 		FVulFieldSerializationContext Ctx;
-// 		TSharedPtr<FVulFieldDescription> Desc = MakeShared<FVulFieldDescription>();
-// 		VTC_MUST_EQUAL(false, Ctx.Describe<FMyCustomType>(Desc), "describe failed");
-//
-// 		CtxContainsError(TC, Ctx.State.Errors, "did not produce a valid description");
-// 		CtxContainsError(TC, Ctx.State.Errors, "FMyCustomType");
-// 	});
-// 	
-// 	VulTest::Case(this, "Schema generation - other common types", [](VulTest::TC TC)
-// 	{
-// 		TOptional<FString> Optional;
-// 		TSharedPtr<FString> SharedPtr;
-// 		TUniquePtr<FString> UniquePtr;
-// 		TPair<FString, FString> Pair;
-// 		TPair<FString, int> DifferingPair;
-// 		FString* Ptr;
-// 		FGuid Guid;
-// 		FName Name;
-// 		float Float;
-// 		
-// 		FVulFieldSet Set;
-// 		Set.Add(FVulField::Create(&Optional), "optional");
-// 		Set.Add(FVulField::Create(&SharedPtr), "sharedPtr");
-// 		Set.Add(FVulField::Create(&UniquePtr), "uniquePtr");
-// 		Set.Add(FVulField::Create(&Pair), "pair");
-// 		Set.Add(FVulField::Create(&DifferingPair), "differingPair");
-// 		Set.Add(FVulField::Create(&Ptr), "ptr");
-// 		Set.Add(FVulField::Create(&Guid), "guid");
-// 		Set.Add(FVulField::Create(&Name), "name");
-// 		Set.Add(FVulField::Create(&Float), "float");
-// 		
-// 		FVulFieldSerializationContext Ctx;
-// 		TSharedPtr<FVulFieldDescription> Desc = MakeShared<FVulFieldDescription>();
-// 		VTC_MUST_EQUAL(true, TestDescribe(TC, Set, Ctx, Desc), "");
-//
-// 		const auto JsonSchema = VulRuntime::Field::JsonToString(Desc->JsonSchema());
-//
-// 		FString Expected = R"(
-// {
-//   "type": "object",
-//   "properties": {
-//     "optional": {"type": ["string", "null"]},
-//     "sharedPtr": {"type": ["string", "null"]},
-//     "uniquePtr": {"type": ["string", "null"]},
-//     "pair": {
-//       "type": "array",
-//       "items": {"type": "string"}
-//     },
-//     "differingPair": {
-//       "type": "array",
-//       "items": {
-//         "oneOf": [
-//           {"type": "string"},
-//           {"type": "number"}
-//         ]
-//       }
-//     },
-//     "ptr": {"type": ["string", "null"]},
-//     "guid": {"type": "string"},
-//     "name": {"type": "string"},
-//     "float": {"type": "number"}
-//   }
-// })";
-// 		
-// 		(void)TC.JsonObjectsEqual(JsonSchema, Expected);
-// 	});
-// 	
-// 	VulTest::Case(this, "Schema generation - enum", [](VulTest::TC TC)
-// 	{
-// 		FVulFieldSerializationContext Ctx;
-// 		TSharedPtr<FVulFieldDescription> Desc = MakeShared<FVulFieldDescription>();
-// 		VTC_MUST_EQUAL(true, TestDescribe<EVulFieldTestTreeNodeType>(TC, Ctx, Desc), "");
-//
-// 		const auto JsonSchema = VulRuntime::Field::JsonToString(Desc->JsonSchema());
-//
-// 		FString Expected = R"(
-// {
-//   "type": "string",
-//   "enum": [
-//     "Base",
-//     "Node1",
-//     "Node2"
-//   ]
-// })";
-// 		
-// 		(void)TC.JsonObjectsEqual(JsonSchema, Expected);
-// 	});
+	VulTest::Case(this, "Schema generation - basic object", [](VulTest::TC TC)
+	{
+		FVulFieldSerializationContext Ctx;
+		TSharedPtr<FVulFieldDescription> Desc = MakeShared<FVulFieldDescription>();
+		VTC_MUST_EQUAL(true, TestDescribe<FVulTestFieldType>(TC, Ctx, Desc), "");
+
+		const auto JsonSchema = VulRuntime::Field::JsonToString(Desc->JsonSchema());
+
+		FString Expected = R"(
+{
+	"type": "object",
+	"properties": {
+		"bool": {"type": "boolean"},
+		"int": {"type": "number"},
+		"string": {"type": "string"},
+		"map": {
+			"type": "object",
+			"additionalProperties": {"type": "number"}
+		},
+		"array": {
+			"type": "array",
+			"items": {"type": "boolean"}
+		}
+	}
+})";
+		
+		(void)TC.JsonObjectsEqual(JsonSchema, Expected);
+	});
+	
+	VulTest::Case(this, "Schema generation - nested objects", [](VulTest::TC TC)
+	{
+		FVulFieldSerializationContext Ctx;
+		TSharedPtr<FVulFieldDescription> Desc = MakeShared<FVulFieldDescription>();
+		VTC_MUST_EQUAL(true, TestDescribe<FVulTestFieldParent>(TC, Ctx, Desc), "");
+
+		const auto JsonSchema = VulRuntime::Field::JsonToString(Desc->JsonSchema());
+
+		FString Expected = R"(
+{
+	"type": "object",
+	"properties": {
+		"inner": {
+			"type": "object",
+			"properties": {
+				"bool": {"type": "boolean"},
+				"int": {"type": "number"},
+				"string": {"type": "string"},
+				"map": {
+					"type": "object",
+					"additionalProperties": {"type": "number"}
+				},
+				"array": {
+					"type": "array",
+					"items": {"type": "boolean"}
+				}
+			}
+		}
+	}
+})";
+		
+		(void)TC.JsonObjectsEqual(JsonSchema, Expected);
+	});
+	
+	VulTest::Case(this, "Schema generation - type cannot be described", [](VulTest::TC TC)
+	{
+		class FMyCustomType : FString { };
+		
+		FVulFieldSerializationContext Ctx;
+		TSharedPtr<FVulFieldDescription> Desc = MakeShared<FVulFieldDescription>();
+		VTC_MUST_EQUAL(false, Ctx.Describe<FMyCustomType>(Desc), "describe failed");
+
+		CtxContainsError(TC, Ctx.State.Errors, "did not produce a valid description");
+		CtxContainsError(TC, Ctx.State.Errors, "FMyCustomType");
+	});
+	
+	VulTest::Case(this, "Schema generation - other common types", [](VulTest::TC TC)
+	{
+		TOptional<FString> Optional;
+		TSharedPtr<FString> SharedPtr;
+		TUniquePtr<FString> UniquePtr;
+		TPair<FString, FString> Pair;
+		TPair<FString, int> DifferingPair;
+		FString* Ptr;
+		FGuid Guid;
+		FName Name;
+		float Float;
+		
+		FVulFieldSet Set;
+		Set.Add(FVulField::Create(&Optional), "optional");
+		Set.Add(FVulField::Create(&SharedPtr), "sharedPtr");
+		Set.Add(FVulField::Create(&UniquePtr), "uniquePtr");
+		Set.Add(FVulField::Create(&Pair), "pair");
+		Set.Add(FVulField::Create(&DifferingPair), "differingPair");
+		Set.Add(FVulField::Create(&Ptr), "ptr");
+		Set.Add(FVulField::Create(&Guid), "guid");
+		Set.Add(FVulField::Create(&Name), "name");
+		Set.Add(FVulField::Create(&Float), "float");
+		
+		FVulFieldSerializationContext Ctx;
+		TSharedPtr<FVulFieldDescription> Desc = MakeShared<FVulFieldDescription>();
+		VTC_MUST_EQUAL(true, TestDescribe(TC, Set, Ctx, Desc), "");
+
+		const auto JsonSchema = VulRuntime::Field::JsonToString(Desc->JsonSchema());
+
+		FString Expected = R"(
+{
+  "type": "object",
+  "properties": {
+    "optional": {"type": ["string", "null"]},
+    "sharedPtr": {"type": ["string", "null"]},
+    "uniquePtr": {"type": ["string", "null"]},
+    "pair": {
+      "type": "array",
+      "items": {"type": "string"}
+    },
+    "differingPair": {
+      "type": "array",
+      "items": {
+        "oneOf": [
+          {"type": "string"},
+          {"type": "number"}
+        ]
+      }
+    },
+    "ptr": {"type": ["string", "null"]},
+    "guid": {"type": "string"},
+    "name": {"type": "string"},
+    "float": {"type": "number"}
+  }
+})";
+		
+		(void)TC.JsonObjectsEqual(JsonSchema, Expected);
+	});
+	
+	VulTest::Case(this, "Schema generation - enum", [](VulTest::TC TC)
+	{
+		FVulFieldSerializationContext Ctx;
+		TSharedPtr<FVulFieldDescription> Desc = MakeShared<FVulFieldDescription>();
+		VTC_MUST_EQUAL(true, TestDescribe<EVulFieldTestTreeNodeType>(TC, Ctx, Desc), "");
+
+		const auto JsonSchema = VulRuntime::Field::JsonToString(Desc->JsonSchema());
+
+		FString Expected = R"(
+{
+  "type": "string",
+  "enum": [
+    "Base",
+    "Node1",
+    "Node2"
+  ]
+})";
+		
+		(void)TC.JsonObjectsEqual(JsonSchema, Expected);
+	});
 	
 	VulTest::Case(this, "Schema generation - inheritance tree", [](VulTest::TC TC)
 	{
