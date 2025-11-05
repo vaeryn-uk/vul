452be9783d3e827c67ac1a7eca61282792c426dc
Andy Cook <andrewcook1990@gmail.com>
2025-11-05 01:00:36 +0100

    Fix: access server-spawned actor from client
    Loop assumed actors were valid, but when replicated down they might not
    yet be.

--- Source/VulRuntime/Public/LevelManager/VulLevelManager.h
+++ Source/VulRuntime/Public/LevelManager/VulLevelManager.h
@@ -590,7 +590,7 @@ ActorClass* UVulLevelManager::GetLevelActor() const
 	{
 		for (const auto& Entry : PrimaryData->ServerSpawnedActors)
 		{
-			if (Entry.Actor->IsA<ActorClass>())
+			if (IsValid(Entry.Actor) && Entry.Actor->IsA<ActorClass>())
 			{
 				return Cast<ActorClass>(Entry.Actor);
 			}
